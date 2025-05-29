a815856ffb1ce0cb2b4861640e137cc39980acc3
JemalJaquavius <acrause@learn68.ca>
2025-05-29 13:15:34 -0700

    Update mainFile.java

--- mainFile.java
+++ mainFile.java
@@ -1,3 +1,14 @@
 public class mainFile {
-  //Adding code later, currently too busy
+  int x = 5;
+  String word = "This is from mainFile";
+  String test = "This is also from mainFile";
+  public void aPublicMethod(){
+    int y = 10;
+    System.out.println(y + " is a number")
+  }
+  public static void main(String[] args) {
+    System.out.println("This right here, is a line of text");
+    Main myObj = new Main();
+    myObj.aPublicMethod();
+  }
 }
