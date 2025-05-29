7c18db0ea601454d9ced4f4cb401ed5df0638bef
JemalJaquavius <acrause@learn68.ca>
2025-05-29 13:34:14 -0700

    Update secondFile.java

--- secondFile.java
+++ secondFile.java
@@ -6,5 +6,16 @@ public class secondFile {
     System.out.println("This is another java file");
     Main myObj = new Main();
     myObj.x();
+    System.out.println("A line of text");
+    int k = 7;
+    if (10 + k == 17){
+      System.out.println("17 is a number");
+    } else {
+      System.out.println("17 is no longer a number");
+    }
+    myObj.word(); 
+    myObj.test();
+    System.out.println(k + "is a number");
+    staticMethod();
   }
 }
