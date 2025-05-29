ecf36aa3f91e5b97ed26b0cf6436a102da3c10f9
JemalJaquavius <acrause@learn68.ca>
2025-05-29 13:30:18 -0700

    Update secondFile.java

--- secondFile.java
+++ secondFile.java
@@ -1,3 +1,10 @@
 public class secondFile {
-  
+  static void staticMethod() {
+    System.out.println("This is a method");
+  }
+  public static void main(String[] args) {
+    System.out.println("This is another java file");
+    Main myObj = new Main();
+    myObj.x();
+  }
 }
