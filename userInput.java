b6d697d13bc9807c3f939f2a7fb5060e769fe630
JemalJaquavius <acrause@learn68.ca>
2025-05-29 17:18:36 -0700

    Update userInput.java

--- userInput.java
+++ userInput.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 class userInput {
   public static void main(String[] args) {
-    Scanner Input = new Scanner(System.in);
+    Scanner nameOfUser = new Scanner(System.in);
     System.out.println("enter username:");
 
-    String username = Input.nextLine();
-    System.out.println("Username is " + username);
+    String username = nameofUser.nextLine();
+    System.out.println("Welcome, " + username);
+
+    Scanner Numbers = new Scanner(System.in);
+    System.out.println("Enter two numbers");
+    String number = Numbers.nextLine();
   }
 }
