7381684e61b7ab836e6eb19ccbef1454bfdee8be
JemalJaquavius <acrause@learn68.ca>
2025-05-30 18:11:07 -0700

    Update userInput2.java

--- userInput2.java
+++ userInput2.java
@@ -4,6 +4,6 @@ class userInput2 {
   public static void Main(String[] args) {
     Scanner Password = new Scanner(System.in)
       System.out.println("Enter password")
-      
+      String passwordInput = 
   }
 }
