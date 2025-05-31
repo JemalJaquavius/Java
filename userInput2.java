8bba2b05f9c7684a7c87714642a5218de1c392b6
JemalJaquavius <acrause@learn68.ca>
2025-05-30 18:41:49 -0700

    Update userInput2.java

--- userInput2.java
+++ userInput2.java
@@ -4,9 +4,12 @@ class userInput2 {
   public static void main(String[] args) {
     Scanner Password = new Scanner(System.in);
       System.out.println("Enter 5-digit password:");
-      String passwordInput = Password.nextLine();
+      String passwordInput = Password.nextInt();
     
-    
+    if (passwordInput == 1875) {
+      System.out.println("Correct password");
+    } else {
+      System.out.println("Incorrect password");
+    }
     }
   }
-}
